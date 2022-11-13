08c82d85911ad1dfa1a5ef84551dbd5be66787cc
Hussein Srour <husseinfo@mail.com>
2022-11-13 15:07:14 +0100

    time range widget

--- app/src/main/java/io/github/husseinfo/interfast/MainActivity.kt
+++ app/src/main/java/io/github/husseinfo/interfast/MainActivity.kt
@@ -1,11 +1,48 @@
 package io.github.husseinfo.interfast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import nl.joery.timerangepicker.TimeRangePicker
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var picker: TimeRangePicker
+    private lateinit var endTime: TextView
+    private lateinit var startTime: TextView
+    private lateinit var duration: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        picker = findViewById(R.id.trp_sleeping)
+        startTime = findViewById(R.id.start_time)
+        endTime = findViewById(R.id.end_time)
+        duration = findViewById(R.id.tv_sleeping_duration)
+
+        updateTimes()
+        updateDuration()
+
+        picker.setOnTimeChangeListener(object : TimeRangePicker.OnTimeChangeListener {
+            override fun onStartTimeChange(startTime: TimeRangePicker.Time) {
+                updateTimes()
+            }
+
+            override fun onEndTimeChange(endTime: TimeRangePicker.Time) {
+                updateTimes()
+            }
+
+            override fun onDurationChange(duration: TimeRangePicker.TimeDuration) {
+                updateDuration()
+            }
+        })
+    }
+
+    private fun updateDuration() {
+        duration.text = getString(R.string.duration, picker.duration)
+    }
+
+    private fun updateTimes() {
+        endTime.text = picker.endTime.toString()
+        startTime.text = picker.startTime.toString()
     }
 }
