6ccb58abf392feb025e601ef9c4b974bac245f70
Hussein Srour <husseinfo@mail.com>
2022-11-13 17:11:28 +0100

    calc eating period

--- app/src/main/java/io/github/husseinfo/interfast/MainActivity.kt
+++ app/src/main/java/io/github/husseinfo/interfast/MainActivity.kt
@@ -1,26 +1,41 @@
 package io.github.husseinfo.interfast
 
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatSeekBar
 import nl.joery.timerangepicker.TimeRangePicker
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var picker: TimeRangePicker
-    private lateinit var endTime: TextView
+    private val methods = arrayOf("16/8", "18/6", "20/4", "22/2", "23/1")
+
     private lateinit var startTime: TextView
+    private lateinit var endTime: TextView
+    private lateinit var startAt: TextView
+    private lateinit var endAt: TextView
     private lateinit var duration: TextView
+    private lateinit var method: TextView
+    private lateinit var picker: TimeRangePicker
+    private lateinit var seekbar: AppCompatSeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         picker = findViewById(R.id.trp_sleeping)
         startTime = findViewById(R.id.start_time)
         endTime = findViewById(R.id.end_time)
+        startAt = findViewById(R.id.tv_breakfast_time)
+        endAt = findViewById(R.id.tv_fast_time)
         duration = findViewById(R.id.tv_sleeping_duration)
+        method = findViewById(R.id.tv_method_selected)
+        seekbar = findViewById(R.id.sb_method)
 
         updateTimes()
         updateDuration()
+        updateMethod()
 
         picker.setOnTimeChangeListener(object : TimeRangePicker.OnTimeChangeListener {
             override fun onStartTimeChange(startTime: TimeRangePicker.Time) {
@@ -35,6 +50,16 @@ class MainActivity : AppCompatActivity() {
                 updateDuration()
             }
         })
+
+        seekbar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(sb: SeekBar?, progress: Int, p2: Boolean) {
+                updateMethod()
+            }
+
+            override fun onStartTrackingTouch(sb: SeekBar?) {}
+
+            override fun onStopTrackingTouch(sb: SeekBar?) {}
+        })
     }
 
     private fun updateDuration() {
@@ -44,5 +69,28 @@ class MainActivity : AppCompatActivity() {
     private fun updateTimes() {
         endTime.text = picker.endTime.toString()
         startTime.text = picker.startTime.toString()
+        calculatePeriod()
+    }
+
+    private fun updateMethod() {
+        method.text = methods[seekbar.progress]
+        calculatePeriod()
+    }
+
+    private fun calculatePeriod() {
+        val eatingHours: Long = when (seekbar.progress) {
+            4 -> {
+                1
+            }
+            else -> {
+                8L - (seekbar.progress * 2)
+            }
+        }
+
+        val awakeDuration = TimeRangePicker.TimeDuration(picker.endTime, picker.startTime)
+        val midEatingTime = picker.endTime.localTime.plusHours(awakeDuration.hour.toLong() / 2)
+
+        startAt.text = midEatingTime.minusHours(eatingHours / 2).toString()
+        endAt.text = midEatingTime.plusHours(eatingHours / 2).toString()
     }
 }
