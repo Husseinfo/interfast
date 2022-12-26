a41fb4a7b814c8dd5c041917a42047ca1663d2cb
Hussein Srour <husseinfo@mail.com>
2022-12-26 20:57:32 +0100

    github actions -> assemble release

--- app/src/main/java/io/github/husseinfo/interfast/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/husseinfo/interfast/ui/home/HomeFragment.kt
@@ -23,7 +23,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
--- app/src/main/java/io/github/husseinfo/interfast/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/github/husseinfo/interfast/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Coming Soon!"
     }
     val text: LiveData<String> = _text
 }
